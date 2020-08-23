b8c4f2d031a19c8a59d38fc448e662cad7f8364b
Chris Scott <scott.chriss@gmail.com>
2020-08-23 14:18:49 -0400

    add serverjs file to the notetaker app

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+//1 require the express module
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-
+//2 create an instance of the express app
 const app = express();
 
+//3 specify port for express to use
 const PORT = process.env.PORT || 2500;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//4 listen for requests on the app to the stated port.
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+
+//module.exports = app;
